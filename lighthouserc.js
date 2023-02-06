d13dbc4a0bc5a5dde2d1763e7a6449782225f316
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-06 18:54:35 -0500

    Trying to fix linter error

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,11 @@
 // lighthouserc.js
 module.exports = {
   ci: {
-        collect: {
-            startServerCommand: 'npm run server',
-            url: ['http://localhost:8080/'],
-      startServerReadyPattern: 'Server is running on PORT 4000',
-      startServerReadyTimeout: 20000,// milliseconds
+    collect: {
+      startServerCommand: 'npm run server',
+      url: ['http://localhost:8080/'],
+      startServerReadyPattern: 'Server is running on PORT 8080',
+      startServerReadyTimeout: 80000, // milliseconds
       numberOfRuns: 5,
     },
     upload: {
