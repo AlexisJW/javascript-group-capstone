4b33b1891efd4d34facdbd072c7a25d9669e9f92
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-06 18:31:47 -0500

    Trying to fix linter error

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import myBackground from './play.jpg';
-
-window.addEventListener('DOMContentLoaded', () => {
-  const backGroundImg = document.getElementsByTagName('body');
-  backGroundImg.src = myBackground;
-});
