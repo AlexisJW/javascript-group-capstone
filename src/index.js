505b7eb58f64bf8eb0b0562968917fb8530ad395
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-06 15:07:31 -0500

    Set up the project with Webpack, linters and Jest

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import myBackground from './play.jpg';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const backGroundImg = document.getElementsByTagName('body');
+  backGroundImg.src = myBackground;
+});
