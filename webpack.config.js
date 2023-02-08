82d8498b842f19b77da1cf0e4c7881b23287a2a4
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-02-08 09:47:49 -0500

    Trying to fix linters errors again

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: './main.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
