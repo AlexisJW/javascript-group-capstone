ddc43d2e2e6f996db339fb53165c24ad6845f74c
Alhassan Osman <oalhassan847@gmail.com>
2023-02-10 20:03:36 +0000

    Finish add comments functionality

--- modules/addComments.js
+++ modules/addComments.js
@@ -2,19 +2,23 @@ import showComments from './showComments.js';
 
 const addComments = async (itemId) => {
     const user = document.querySelector('#username');
-    const comment = document.querySelector('#username');
+    const comment = document.querySelector('#comment');
     fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/X2HxEwPkDolJVV1WCgVc/comments', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify({
-      item_id: itemId,
-      username: user.value,
-      comment: comment.value,
-    }),
-  })
-  
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            item_id: itemId,
+            username: user.value,
+            comment: comment.value,
+        }),
+    })
+        .then(() => {
+            showComments(itemId);
+            user.value = '';
+            comment.value = '';
+        });
 };
 
 export default addComments;
